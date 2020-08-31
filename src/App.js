95cf9aebcbb7aa4e547dca61ab2123dda70eb537
MCauz <michaelhcausey@gmail.com>
2020-08-31 11:00:24 -0400

    11a Monday

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
 
-const baseUrl = 'http://localhost:3000'
+const baseUrl = 'http://localhost:3000/users'
 
 class App extends React.Component {
 
   componentDidMount() {
-    fetch
+    fetch(baseUrl)
+    .then(resp => resp.json())
+    .then(console.log)
   }
 
   render() {
     return (
       <div>
-        <h1>Home Page</h1>
+        <h1>NavBar</h1>
 
       </div>
       
