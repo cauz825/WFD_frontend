bd9a43187023897272559cd003813087ff5fda42
MCauz <michaelhcausey@gmail.com>
2020-09-04 13:59:33 -0400

    recipe call api set

--- src/containers/RecipeContainer.js
+++ src/containers/RecipeContainer.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 function RecipeContainer() {
+    
+    function searchReceipes() {
+        fetch("http://localhost:3000/receipe_call", {
+            method: "POST",
+            headers:{"Content-Type":"application/json"},
+            body: JSON.stringify({
+                user: localStorage.user_id
+            })
+        })
+    }
+    
+    
     return(
         <div>
             <h4>Recipe Container</h4>
-            <button>Search for Recipes</button>
+            <button onClick={searchReceipes}>Search for Recipes</button>
         </div>
     )
 }
